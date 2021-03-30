4b57cd284ca0fc3acc8b0e93da8f9fcd9e23b75c
rmzNadir <dgzrz99@gmail.com>
2021-03-29 21:40:31 -0600

    :bug: fixed cors issues

--- patientor-back/src/index.ts
+++ patientor-back/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 app.use(express.json());
 
@@ -12,6 +17,8 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
+
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
