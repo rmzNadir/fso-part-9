e845c9588e0d0f6d5a458dbd71e09176aee960a0
rmzNadir <dgzrz99@gmail.com>
2021-04-03 22:32:24 -0600

    Exercise 9.17

---
+++ patientor-front/src/PatientPage/index.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
+
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+import { updatePatient } from '../state/reducer';
+import { Patient } from '../types';
+
+const PatientPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patients }, dispatch] = useStateValue();
+  const [isLoading, setIsLoading] = useState(false);
+  const lastFetched = useRef<string>('');
+
+  useEffect(() => {
+    const getPatient = async () => {
+      setIsLoading(true);
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch(updatePatient(patientFromApi));
+        lastFetched.current = id;
+      } catch (e) {
+        console.log(e);
+      }
+      setIsLoading(false);
+    };
+
+    if (lastFetched.current !== id) {
+      void getPatient();
+    }
+  }, [id, dispatch]);
+
+  const patient = patients[id];
+  // console.log('patient', patient);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (!patient) {
+    return (
+      <div>
+        <h2>Patient not found!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {
+        <h2>
+          {patient.name}{' '}
+          <Icon
+            name={
+              patient.gender === 'female'
+                ? 'venus'
+                : patient.gender === 'male'
+                ? 'mars'
+                : 'genderless'
+            }
+          />
+        </h2>
+      }
+      <div>ssn: {patient.ssn}</div>
+      <div>occupation: {patient.occupation}</div>
+    </div>
+  );
+};
+
+export default PatientPage;
